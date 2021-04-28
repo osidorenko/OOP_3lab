b9e66997b31922915bf6335d3a7b04f355f9615e
osidorenko <so.sidorenko@yandex.ru>
2021-04-28 14:20:38 +0300

    last

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -32,9 +32,6 @@ public class Main extends Application {
 
         Scooter s = new Scooter("Xiaomi", 30);
         s.setMaxSpeed(35);
-        //String ss = s.Serelize();
-        //Scooter v = (Scooter) s.deSerelize(ss);
-        //System.out.println(v.getMaxSpeed());
         ArrayList<Vehicle> list = new ArrayList<>();
         LightVehicle vihicle = new LightVehicle("rapid - 2017", 240, true, 120, bodyType.sedan);
         vihicle.setFirm("Skoda");
@@ -45,59 +42,6 @@ public class Main extends Application {
         list.add(vihicle);
         list.add(s);
 
-
-       // XMLList list1 = new XMLList();
-       // list1.vihecles = list;
-        //list1.vihecles.add(list.get(0));
-       //list1.vihecles.add(list.get(1));
-
-        //!!!!!!!!!todo  YAML
-        try {
-            //String s ="";
-
-            //ObjectMapper om = new ObjectMapper(new YAMLFactory());
-            //String sss = om.writeValueAsString(list);
-            //om.writeValue(new File("C:\\tmp\\Vehicle.yaml"), list);
-            //    System.out.println(sss);
-            //om = null;
-        } catch (Exception e) {
-            System.out.println("hello");
-            e.printStackTrace();
-
-        }
-        //todo Binary
-        try {
-            //ObjectMapper mapper = new ObjectMapper(new Bson);
-            ByteArrayOutputStream b = new ByteArrayOutputStream();
-            ObjectOutputStream stream = new ObjectOutputStream(b);
-            stream.close();
-            stream.writeObject(list);
-            System.out.println();
-            ObjectInputStream stream2 = new ObjectInputStream(new ByteArrayInputStream(b.toByteArray()));
-            ArrayList<Vehicle> vih = (ArrayList<Vehicle>) stream2.readObject();
-            stream2.close();
-            //System.out.println(vih.get(1).getName());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        //todo XML
-        try {
-            //JaxbAnnotationModule jaxbAnnotationModule = new JaxbAnnotationModule();
-            XmlMapper m = new XmlMapper();
-
-            //mapper.enable(SerializationFeature.INDENT_OUTPUT);
-            //mapper.registerModule(jaxbAnnotationModule);
-            //mapper.registerModule(new GuavaModule());
-
-
-            m.writeValue(new File("C:\\tmp\\Vehicle2.xml"),list);
-            //String out = map.writeValueAsString(new LightVehicle());
-            //System.out.println(out);
-            ArrayList ss = m.readValue(new File("C:\\tmp\\Vehicle2.xml"),ArrayList.class);
-            System.out.println("fuckyou");
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
 
     }
 }
